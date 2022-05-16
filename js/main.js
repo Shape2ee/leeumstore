164afa22839b7ebd042a464310a1645482d476c4
shape2ee <shape1026@gmail.com>
2022-05-16 12:39:03 +0900

    javascript 파일 추가

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// function hambergerMenu() {
+//     let element = document.querySelector(".hamberger_menu");
+//     element.classList.toggle("active");
+//   }
+
+  function open_sideNav() {
+    document.querySelector("aside").style.width = "300px";
+  }
+
+  function close_sideNav() {
+    document.querySelector("aside").style.width = "0";
+  }
