3527dcc61cc5fcb388ccb9bc6b4634f153d8f3fb
Shape Lee <shape1026@gmail.com>
2022-05-16 23:52:11 +0900

    220516 darkMode add

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-  function openNav() {
-    let sideNav = document.querySelector("aside")
-    sideNav.classList.toggle("open");
+function openNav() {
+  let sideNav = document.querySelector("aside");
+  sideNav.classList.toggle("open");
+}
+
+function darkMode() {
+  let body = document.body;
+  body.classList.toggle("dark");
+
+  let button = document.getElementById("theme_btn");
+  if (button.innerHTML == "light mode") {
+    button.innerHTML = "dark mode";
+  } else {
+    button.innerHTMl = "light mode";
   }
+}
