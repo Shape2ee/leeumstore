9f3d10141ef6d3f73f90bf0307b63836ef7f2fc2
Shape Lee <shape1026@gmail.com>
2022-05-18 22:37:18 +0900

    220518 dark mode 수정

--- js/main.js
+++ js/main.js
@@ -3,14 +3,27 @@ function openNav() {
   sideNav.classList.toggle("open");
 }
 
-function darkMode() {
-  let body = document.body;
-  body.classList.toggle("dark");
-
-  let button = document.getElementById("theme_btn");
-  if (button.innerHTML == "light mode") {
-    button.innerHTML = "dark mode";
-  } else {
-    button.innerHTMl = "light mode";
-  }
+// 로컬스토리지의 다크모드 상태 읽기
+if (localStorage.getItem("theme") === "dark") {
+  document.documentElement.dataset.theme = "dark";
 }
+
+const button = document.getElementById("theme_tit");
+
+document.getElementById("theme_btn").addEventListener("click", function () {
+  if (document.documentElement.dataset.theme === "light") {
+    // 다크모드 설정
+    document.documentElement.dataset.theme = "dark";
+    // 다크모드 유지
+    localStorage.setItem("theme", "dark");
+    // 버튼 텍스트 다크모드로 변경
+    button.innerHTML = "다크모드";
+  } else {
+    // 다크모드 설정 해지
+    document.documentElement.dataset.theme = "light";
+    // 로컬스토리지에 다크모드 해제 저장
+    localStorage.setItem("theme", "light");
+    // 버튼 텍스트 라이트모드로 변경
+    button.innerHTML = "라이트모드";
+  }
+});
